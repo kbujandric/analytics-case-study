75a1452fd29fb597144007fd0f099c2d9dde03b5
Katarina Bujandrić <74470169+kbujandric@users.noreply.github.com>
2026-03-11 21:34:43 +0100

    created int_derive_mrr
    intermediate model for calculating net revenue and mrr

---
+++ models/intermediate/int_derive_mrr.sql
@@ -0,0 +1,5 @@
+SELECT
+    *,
+    (gross_amount - gross_amount * tax_percentage) * exchange_rate AS net_revenue,
+    (gross_amount - gross_amount * tax_percentage) * exchange_rate / 12 AS mrr
+FROM {{ ref('stg_orders') }}
