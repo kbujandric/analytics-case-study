a860140d44c1d092ddd9add2acc982ebf84ecb7a
Katarina Bujandrić <74470169+kbujandric@users.noreply.github.com>
2026-03-13 20:48:21 +0100

    indentation mart_mrr_movements_monthly

--- models/mart/mart_mrr_movements_monthly.sql
+++ models/mart/mart_mrr_movements_monthly.sql
@@ -1,12 +1,12 @@
 SELECT
-  month,
-  SUM(start_mrr) AS mrr_start_of_period,
-  SUM(mrr_change) AS mrr_change,
-  SUM(end_mrr) AS mrr_end_of_period,
-  SUM(CASE WHEN change_type = 'new' THEN mrr_change END) AS mrr_new,
-  SUM(CASE WHEN change_type = 'expansion' THEN mrr_change END) AS mrr_expansion,
-  SUM(CASE WHEN change_type = 'contraction' THEN mrr_change END) AS mrr_contraction,
-  SUM(CASE WHEN change_type = 'lost' THEN mrr_change END) AS mrr_lost
+    month,
+    SUM(start_mrr) AS mrr_start_of_period,
+    SUM(mrr_change) AS mrr_change,
+    SUM(end_mrr) AS mrr_end_of_period,
+    SUM(CASE WHEN change_type = 'new' THEN mrr_change END) AS mrr_new,
+    SUM(CASE WHEN change_type = 'expansion' THEN mrr_change END) AS mrr_expansion,
+    SUM(CASE WHEN change_type = 'contraction' THEN mrr_change END) AS mrr_contraction,
+    SUM(CASE WHEN change_type = 'lost' THEN mrr_change END) AS mrr_lost
 FROM {{ ref("int_mrr_movements") }}
 GROUP BY month
 ORDER BY month
