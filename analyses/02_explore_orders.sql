d087d1666b3ba75ee51804630450a47fad5ef00e
Katarina Bujandrić <74470169+kbujandric@users.noreply.github.com>
2026-03-14 12:50:53 +0100

    added files with data exploration code
    separate for subscriptions, orders and joins

---
+++ analyses/02_explore_orders.sql
@@ -0,0 +1,84 @@
+------------------------------------------
+-- DUPLICATION
+------------------------------------------
+SELECT 
+    *
+FROM (
+    SELECT 
+        *,
+        COUNT(*) OVER (PARTITION BY subscription_id) AS num_rows
+    FROM {{ source("ninox", "orders") }}
+)
+WHERE num_rows > 1
+ORDER BY num_rows DESC
+-- duplication is present
+
+-- how many order_ids are not unique?
+SELECT
+  order_id,
+  COUNT(*) AS num_rows
+FROM {{ source("ninox", "orders") }}
+GROUP BY order_id
+HAVING num_rows > 1;
+-- 11 order_ids with multiple rows in orders
+
+
+-- are these rows *complete* duplicates?
+-- i.e., are all columns in duplicated orders the same
+
+-- if all count(distinct column) columns = 1, that means that the duplicate rows are fully identical
+-- advantage: able to see exactly in which columns the "duplicate" rows are potentially different
+SELECT
+    order_id,
+    COUNT(*) AS total_rows, -- number of rows with same order_id (grouped by)
+    COUNT(DISTINCT subscription_id) AS distinct_subscription_id,
+    COUNT(DISTINCT order_date) AS distinct_order_date,
+    COUNT(DISTINCT gross_amount) AS distinct_gross_amount,
+    COUNT(DISTINCT checkout_metadata) AS distinct_checkout_metadata,
+FROM {{ source("ninox", "orders") }}
+GROUP BY order_id
+HAVING total_rows > 1 -- find those order_ids that appear more than once
+-- all DISTINCT columns = 1, meaning deduplication is safe and necessary
+-- could be verified by CTE, counting those where any of DISTINCT columns > 1
+
+ ------------------------------------------
+-- MISSINGNESS 
+------------------------------------------
+SELECT
+    COUNTIF(order_id IS NULL) AS order_id, 
+    COUNTIF(subscription_id IS NULL) AS subscription_id, 
+    COUNTIF(order_date IS NULL) AS order_date, 
+    COUNTIF(gross_amount IS NULL) AS gross_amount,
+    COUNTIF(checkout_metadata IS NULL) AS checkout_metadata,
+    COUNTIF(JSON_VALUE(checkout_metadata, '$.exchange_rate') IS NULL) AS exchange_rate,
+    COUNTIF(JSON_VALUE(checkout_metadata, '$.tax_percentage') IS NULL) AS tax_percentage,
+    COUNTIF(JSON_VALUE(checkout_metadata, '$.currency') IS NULL) AS currency
+FROM {{ source("ninox", "orders") }}
+-- only missing tax_percentage for 11 rows
+-- how big of a problem is this?
+
+
+-- checking other values of tax_percentage: if often large, 
+-- then MRR could be gravely overestimated
+WITH parsed AS (
+    SELECT
+        subscription_id,
+        order_id,
+        DATE(order_date) AS order_date,
+        gross_amount,
+        CAST(JSON_VALUE(checkout_metadata, '$.exchange_rate') AS FLOAT64) AS exchange_rate,
+        CAST(JSON_VALUE(checkout_metadata, '$.tax_percentage') AS FLOAT64) AS tax_percentage
+    FROM (
+        SELECT 
+            DISTINCT *
+        FROM {{ source("ninox", "orders") }})
+        )
+    )
+SELECT
+    tax_percentage,
+    COUNT(*) AS frequency
+FROM parsed
+GROUP BY tax_percentage
+-- out of >700 orders, 614 have tax_percentage = 0, and only 11 are NULL
+-- I would flag this as an upstream problem but not discard these rows entirely, 
+-- at least for this case study's purposes
---
+++ analyses/03_explore_joins.sql
@@ -0,0 +1,102 @@
+------------------------------------------
+-- MISMATCHING ROWS
+------------------------------------------
+-- combining the tables using full outer join,
+-- then finding those rows that have no matches ("orphans")
+-- and marking them to clarify type of mismatch
+SELECT
+    orders.order_id,
+    subscriptions.subscription_id,
+    CASE
+        WHEN subscriptions.subscription_id IS NULL THEN 'order has no subscription'
+        WHEN orders.order_id IS NULL THEN 'subscription has no order'
+    END AS mismatch_type
+FROM {{ source("ninox", "orders") }} AS orders
+FULL OUTER JOIN {{ source("ninox", "subscriptions") }} AS subscriptions 
+    USING (subscription_id)
+WHERE 
+    orders.order_id IS NULL 
+    OR 
+    subscriptions.subscription_id IS NULL
+-- 2 orphaned orders: have information on transaction, but no information on customer_id
+-- DECISION: drop these rows for these analyses because customer identity is crucial;
+-- MRR movements depend on having continuity *within customer*
+-- MRR cohort retention depend on identifying *customer's* first subscription
+
+
+------------------------------------------
+-- DATE IMPUTATION
+------------------------------------------
+-- some subscriptions are missing start_date and end_date
+-- worth checking whether start_date = order_date for non-missing data
+WITH everything AS (
+    SELECT
+        orders.*, 
+        subscriptions.start_date,
+        subscriptions.end_date
+    FROM {{ source("ninox", "orders") }} AS orders
+    FULL OUTER JOIN {{ source("ninox", "subscriptions") }} AS subscriptions
+    ON orders.subscription_id = subscriptions.subscription_id
+)
+SELECT *
+FROM everything
+WHERE 
+    start_date != DATE(order_date) -- because order_date is datetime
+    OR start_date IS NULL
+    OR order_date IS NULL; 
+-- the order date and subscription start date are always equal for each subscription, 
+-- except when start_date is missing
+-- using order_date that start_date and end_date to impute in order to keep all subscriptions in table
+
+
+------------------------------------------
+-- DATE-RELATED BUSINESS RULE VALIDATION/EXPLORATION 
+------------------------------------------
+
+-- are subscriptions always continuous? or are there sometimes pauses between them?
+    -- despite the task definition at hand,
+    -- I think it's important to think about the possibility of "resurrections",
+    -- i.e., customers that left for a certain period of time and then came back to get a new subscription
+    -- if we treat them as new customers, we are skewing our insights and losing valuable information
+
+
+-- handle order duplications 
+WITH deduped AS (
+    SELECT 
+      DISTINCT *
+    FROM {{ source("ninox", "orders") }}
+),
+
+-- impute order_date as start_date; calculate end_date
+joined AS (
+    SELECT
+        s.customer_id,
+        s.subscription_id,
+        DATE_TRUNC(COALESCE(s.start_date, DATE(d.order_date)), MONTH) AS start_date, -- order_date is DATETIME
+        DATE_TRUNC(COALESCE(s.end_date, DATE_ADD(DATE(d.order_date), INTERVAL 12 MONTH)), MONTH) AS end_date
+    FROM deduped AS d
+    INNER JOIN {{ source("ninox", "subscriptions") }} AS s
+        USING (subscription_id)
+),
+
+-- find end of previous customer's subscription
+previous AS (
+    SELECT
+        *,
+        LAG(end_date) OVER (PARTITION BY customer_id ORDER BY start_month) AS prev_end_date
+    FROM joined
+)
+
+-- compare "next" start date with previous end date
+SELECT *
+FROM previous
+WHERE 
+    start_date != prev_end_date
+ORDER BY customer_id, start_date
+-- no rows returned, meaning we have no resurrections
+
+
+-- can a customer have any simultaneous subscriptions?
+    -- this test is accomplished using the same query as above, just changing WHERE to be more specific:
+    -- WHERE start_date < prev_end_date
+    -- but because the previous query already returned no rows, I won't be writing it out here
