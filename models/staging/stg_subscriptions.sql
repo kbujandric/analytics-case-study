523050299fbcb1e78bb40fff37bdeeb22288e653
Katarina Bujandrić <74470169+kbujandric@users.noreply.github.com>
2026-03-15 13:21:52 +0100

    removed unnecessary columns from stg_subscriptions

--- models/staging/stg_subscriptions.sql
+++ models/staging/stg_subscriptions.sql
@@ -1,8 +1,6 @@
 SELECT
     customer_id,
     subscription_id,
-    LOWER(plan_name) AS plan_name,
-    number_of_licenses,
     start_date,
     end_date
 FROM {{ source("ninox", "subscriptions") }}
